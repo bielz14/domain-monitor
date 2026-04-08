32a01bcf72e58da85ba747a8a80eb4b0c1637302
DESKTOP-BDITDFB\User <slime1410@gmail.com>
2026-04-08 22:25:55 +0300

    move auth routes

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DomainController;
 use App\Http\Controllers\Api\CheckController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->group(function () {
-
-    Route::post('/logout', [AuthController::class, 'logout']);
-
     Route::get('/domains', [DomainController::class, 'index']);
     Route::post('/domains', [DomainController::class, 'store']);
     Route::get('/domains/{domain}', [DomainController::class, 'show']);
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::get('/auth-user', [AuthController::class, 'user']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/auth-user', [AuthController::class, 'user']);
+});
 
 Route::view('/{any?}', 'app')->where('any', '.*');
